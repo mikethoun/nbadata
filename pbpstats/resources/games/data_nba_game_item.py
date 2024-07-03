984f9eb788fdf9ff97ec8ceb8aeed062e60cec8a
Mike Thoun <mikethoun@gmail.com>
2024-07-02 22:35:24 -0500

    add fields

--- pbpstats/resources/games/data_nba_game_item.py
+++ pbpstats/resources/games/data_nba_game_item.py
@@ -1,7 +1,12 @@
 KEY_ATTR_MAPPER = {
     "gid": "game_id",
     "gdte": "date",
+    "utctm": "utc_time",
     "stt": "status",
+    "seri": "series_summary",
+    "an": "stadium",
+    "ac": "stadium_city",
+    "as": "stadium_state",
 }
 
 
@@ -17,10 +22,16 @@ class DataNbaGameItem(object):
             if item.get(key) is not None:
                 setattr(self, value, item.get(key))
         self.home_team_id = item["h"]["tid"]
+        self.home_team_record = item["h"]["re"]
         self.home_team_abbreviation = item["h"]["ta"]
+        self.home_team_name = item["h"]["tn"]
+        self.home_team_city = item["h"]["tc"]
         self.home_score = item["h"]["s"]
         self.away_team_id = item["v"]["tid"]
+        self.away_team_record = item["v"]["re"]
         self.away_team_abbreviation = item["v"]["ta"]
+        self.away_team_name = item["v"]["tn"]
+        self.away_team_city = item["v"]["tc"]
         self.away_score = item["v"]["s"]
 
     @property
